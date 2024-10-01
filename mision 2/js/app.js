344219b5851dc113d3c227a49f2b9b212d8589e5
Antonio810-afk <antoniojoseromeroparada810@gmail.com>
2024-10-01 12:49:28 -0500

    agrege carpeta mision 2

---
+++ mision 2/js/app.js
@@ -0,0 +1,57 @@
+console.log("hola mundo");
+var nombre="Antonio";
+var apellido="Romero";
+var edad="25";
+const PI=3.14159;
+
+console.log(nombre)
+console.log(apellido)
+console.log(PI)
+console.log(nombre)
+
+let PERSONA={
+    nombre:"Antonio",
+    apellido:"Romero",
+}
+console.log(PERSONA)
+console.log(nombre)
+console.log(apellido)
+
+function saludar(){
+    console.log("Hola Grupo");
+}
+saludar();
+
+
+let Numero1=10
+let Numero2=5
+let suma=10+5;
+let resta=Numero1-Numero2
+let multiplicacion=Numero1*Numero2
+let division=Numero1/Numero2
+
+console.log("suma" +" " + suma);
+console.log("suma" + 10 + 5);
+console.log(10 + 5 +"suma");
+console.log("resta"+" "+ resta);
+console.log("multiplicacion"+" "+ multiplicacion);
+console.log("division"+" "+ division);
+
+let x=10
+let y=18
+console.log(x);
+console.log(y);
+console.log(x);
+x +=5;//=>x=x+5 15
+console.log(x);
+x +=5;//=>x=x+5 75
+console.log(x);
+x++;//=>x=x+5   15
+console.log(x);
+x *=5;//=>x=x+5 75
+console.log(x);
+x++;//=>x=x+5   15
+console.log(x !=y);
+console.log(x !=y);
+console.log(x !=y);
+console.log(x !=y);
